fb6452ed1a45e16e6ddd3819c8c19497d02aeb2e
Ruyut <a@ruyut.com>
2022-11-16 00:45:09 +0800

    init commit

---
+++ app/src/main/java/app/ruyut/example/qrcode/MainActivity.java
@@ -0,0 +1,14 @@
+package app.ruyut.example.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
