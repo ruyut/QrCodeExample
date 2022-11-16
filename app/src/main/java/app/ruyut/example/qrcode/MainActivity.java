c3ba39b14898a3a0cb433c020ec931273c3308ff
Ruyut <a@ruyut.com>
2022-11-17 00:00:19 +0800

    feat: 新增 QR Code 掃描

--- app/src/main/java/app/ruyut/example/qrcode/MainActivity.java
+++ app/src/main/java/app/ruyut/example/qrcode/MainActivity.java
@@ -4,13 +4,28 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private TextView textView;
+    private CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +34,60 @@ public class MainActivity extends AppCompatActivity {
 
         // 取得相機權限
         getPermissionCamera();
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        textView = (TextView) findViewById(R.id.textView);
+
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+                if (qrCodes.size() != 0) {
+                    textView.post(() -> textView.setText(qrCodes.valueAt(0).displayValue));
+                }
+            }
+        });
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+//                .setRequestedPreviewSize(300, 300) // 設定預覽視窗大小
+                .setAutoFocusEnabled(true) // 自動對焦
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED)
+                    return;
+                try {
+                    cameraSource.start(surfaceHolder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
     }
 
-    /**
-     * 自訂相機權限代號，用於判斷是否取得權限
-     */
-    private static final int REQUEST_CAMERA_PERMISSION = 1;
 
     /**
      * 取得相機權限
@@ -55,6 +118,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    /**
+     * 自訂相機權限代號，用於判斷是否取得權限
+     */
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+
     /**
      * 取得詢問相機權限的結果
      */
@@ -67,6 +136,17 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // 使用者同意授予權限
                     Toast.makeText(this, "已取得相機權限", Toast.LENGTH_SHORT).show();
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(surfaceView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
                 } else {
                     // 使用者拒絕授予權限
                     Toast.makeText(this, "未取得相機權限", Toast.LENGTH_SHORT).show();
